c919b40009eb1ab7ee0fa551992244585c23cc9f
szabesz <jakab.szabolcs226@gmail.com>
2023-05-29 17:20:59 +0200

    kis bug fixek
    mostmár az adminnak van a pizzákról is crud.
    not sure if needed

---
+++ pizza-parent/pizza-frontend/src/main/java/org/example/mbean/admin/PizzaMBean.java
@@ -0,0 +1,80 @@
+package org.example.mbean.admin;
+
+import jdk.jfr.Name;
+import org.apache.bcel.generic.FLOAD;
+import org.example.entity.Pizza;
+import org.example.mbean.LoginMBean;
+import org.example.service.PizzaService;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class PizzaMBean extends LoginMBean implements Serializable {
+
+    private List<Pizza> pizzaList;
+    private Pizza selectedPizza = new Pizza();
+
+
+    @Inject
+    private PizzaService pizzaService;
+
+    @PostConstruct
+    public void init() {
+        load();
+    }
+
+    private void load() {
+        pizzaList = pizzaService.getAll();
+    }
+
+
+
+    public void save() {
+        if(selectedPizza.getId() == null) {
+            selectedPizza.setCreatorUser(getLoggedInUser());
+            pizzaService.add(selectedPizza);
+        } else {
+            selectedPizza.setModifierUser(getLoggedInUser());
+            selectedPizza.setModificationDate(new java.sql.Timestamp(System.currentTimeMillis()));
+            pizzaService.update(selectedPizza);
+
+        }
+        infoMessage("Pizza saved successfully.");
+        load();
+        initNewPizza();
+
+    }
+
+    public void remove() {
+        pizzaService.remove(selectedPizza);
+        infoMessage("Pizza deleted successfully.");
+        load();
+        initNewPizza();
+    }
+
+    public void initNewPizza() {
+        selectedPizza = new Pizza();
+    }
+
+    public List<Pizza> getPizzaList() {
+        return pizzaList;
+    }
+
+    public void setPizzaList(List<Pizza> pizzaList) {
+        this.pizzaList = pizzaList;
+    }
+
+    public Pizza getSelectedPizza() {
+        return selectedPizza;
+    }
+
+    public void setSelectedPizza(Pizza selectedPizza) {
+        this.selectedPizza = selectedPizza;
+    }
+}
